9de4b43bb01c437d37c56e7bfaca9a4d4e94c21c
alexwhitmore <heyimalexw@gmail.com>
2024-06-28 12:27:17 -0600

    build

--- src/lib/generateToc.ts
+++ src/lib/generateToc.ts
@@ -1,4 +1,4 @@
-// Heavy inspiration from starlight: https://github.com/withastro/starlight/blob/main/packages/starlight/utils/generateToC.ts
+// lib/toc.ts
 import type { MarkdownHeading } from 'astro'
 
 export interface TocItem extends MarkdownHeading {
@@ -17,22 +17,21 @@ function injectChild(items: TocItem[], item: TocItem): void {
     items.push(item)
   } else {
     injectChild(lastItem.children, item)
-    return
   }
 }
 
 export function generateToc(
   headings: ReadonlyArray<MarkdownHeading>,
   { maxHeadingLevel = 4, minHeadingLevel = 2 }: TocOpts = {}
-) {
-  // by default this ignores/filters out h1 and h5 heading(s)
+): TocItem[] {
   const bodyHeadings = headings.filter(
     ({ depth }) => depth >= minHeadingLevel && depth <= maxHeadingLevel
   )
-  const toc: Array<TocItem> = []
+  const toc: TocItem[] = []
 
-  for (const heading of bodyHeadings)
+  for (const heading of bodyHeadings) {
     injectChild(toc, { ...heading, children: [] })
+  }
 
   return toc
 }
