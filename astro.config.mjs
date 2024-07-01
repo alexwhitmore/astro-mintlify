4c5f8257a4764da944b38f7e94299f17db8672f7
alexwhitmore <heyimalexw@gmail.com>
2024-06-30 22:15:21 -0600

    Create ability to create new docs without touching /pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,35 @@ import react from '@astrojs/react'
 import tailwind from '@astrojs/tailwind'
 import vercel from '@astrojs/vercel/serverless'
 import { defineConfig } from 'astro/config'
+import { collections } from './src/config/site.json'
 
 export default defineConfig({
-  output: 'server',
+  output: 'hybrid',
   adapter: vercel({
     webAnalytics: {
       enabled: true,
     },
   }),
   prefetch: true,
-  integrations: [react(), tailwind(), mdx()],
+  integrations: [
+    react(),
+    tailwind(),
+    mdx(),
+    {
+      name: 'generate-pages',
+      hooks: {
+        'astro:config:setup': async ({ injectRoute }) => {
+          injectRoute({
+            pattern: '/',
+            entryPoint: 'src/pages/index.astro',
+          })
+          injectRoute({
+            pattern: '/:type/:slug?',
+            entryPoint: 'src/pages/[type]/[...slug].astro',
+          })
+        },
+      },
+    },
+  ],
+  content: collections,
 })
--- src/content/config.ts
+++ src/content/config.ts
@@ -1,12 +1,16 @@
 import { defineCollection, z } from 'astro:content'
+import { collections as TemplateCollections } from '../config/site.json'
 
-const posts = defineCollection({
-  schema: z.object({
-    title: z.string(),
-    date: z.string().transform((str) => new Date(str)),
-    description: z.string().optional(),
-    draft: z.boolean().optional(),
-  }),
+const docsSchema = z.object({
+  title: z.string(),
+  date: z.string().transform((str) => new Date(str)),
+  description: z.string().optional(),
+  draft: z.boolean().optional(),
 })
 
-export const collections = { posts }
+export const collections = Object.fromEntries(
+  Object.entries(TemplateCollections).map(([key]) => [
+    key,
+    defineCollection({ schema: docsSchema }),
+  ])
+)
