a56c445750d0b94c1873165573f5e889e92284f9
alexwhitmore <heyimalexw@gmail.com>
2024-06-30 22:20:31 -0600

    Fix file path error

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,7 +1,7 @@
 import type { CollectionEntry } from 'astro:content'
-import { contentConfig } from '../../astro.config.mjs'
+import { collections } from '../config/site.json'
 
-export type CollectionKey = keyof typeof contentConfig.collections
+export type CollectionKey = keyof typeof collections
 
 export type ContentEntryMap = {
   [K in CollectionKey]: CollectionEntry<K>
